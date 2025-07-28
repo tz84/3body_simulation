207a2557e5a98bc35d74cbf59d225b0be477e0b9
tzub <89816167+tz84@users.noreply.github.com>
2025-07-28 18:35:37 -0500

    Created header

---
+++ simulation.h
@@ -0,0 +1,16 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+using namespace glm;
+
+#endif
+
