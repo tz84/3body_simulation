98771498c965bdd98f657a6d514bef83da5bbc7a
tzub <89816167+tz84@users.noreply.github.com>
2025-08-05 23:13:34 -0500

    added chrono and thread

--- simulation.h
+++ simulation.h
@@ -5,6 +5,9 @@
 #include <cmath>
 #include <random>
 #include <vector>
+#include <chrono> 
+#include <thread> 
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
